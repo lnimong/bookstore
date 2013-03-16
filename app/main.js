9e367dbb2b1f3f6706a0f3c6a24d30088c76c476
lnimong <lnimong@gmail.com>
2013-03-16 12:57:17 -0700

    returns the index.html

--- app/main.js
+++ app/main.js
@@ -1,12 +1,21 @@
 var express = require('express');
-
+var fs = require('fs');
+var port = process.env.PORT || 5000;
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+
+	app.get('/', function(request, response) {
+	  response.send(html);
+	});
+
+
+	app.listen(port, function() {
+	  console.log("Listening on " + port);
+	});
 });
