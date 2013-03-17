93f655c34264eb5ca9dd70d6507fabdc5a68d590
lnimong <lnimong@gmail.com>
2013-03-17 12:53:49 -0700

    results page template

--- start.js
+++ start.js
@@ -2,10 +2,16 @@ var express = require('express');
 var fs = require('fs');
 var port = process.env.PORT || 5000;
 var app = express.createServer(express.logger());
+var setPublic = function (directories_) {
+	for(var i = 0; i < directories_.length; ++i)
+		app.use('/' + directories_[i], express.static(__dirname + '/' +  directories_[i]));
+};
+
+
 
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/css'));
+setPublic(['css', 'js', 'bootstrap']);
 
 app.get('/', function(request, response) {
   response.render('index', { title: 'The index page!', text : __dirname })
